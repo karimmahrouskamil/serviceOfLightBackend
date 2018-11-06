d9015f35f21bd1a4c5e08aafcf0c5dee9dee0169
karim kamil <kkamil@eg.ibm.com>
2018-11-06 20:51:22 +0200

    data base is connected

--- models/makhdoumin.js
+++ models/makhdoumin.js
@@ -1,7 +1,6 @@
 var mongoose = require("mongoose");
 var MakhdouminSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
     name: String,
   address: String
-});
+}, { collection: 'makhdoumin' });
 module.exports = mongoose.model("makhdoumin", MakhdouminSchema);
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,22 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var Makhdoumin = require('./../models/makhdoumin');
+var Makhdoumin = require("./../models/makhdoumin");
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
 });
-router.get('/test2', function(req, res, next) {
-  
-  Makhdoumin.find(function (err, products) {
+router.get("/test2", function(req, res, next) {
+  Makhdoumin.find(function(err, products) {
     console.log(products);
-    
+
     if (err) return next(err);
     res.json(products);
+  });
+});
+router.post("/addUser", function(req, res, next) {
+  Makhdoumin.create(req.body, function(err, post) {
+    if (err) return next(err);
+    res.json(post);
   });
 });
 module.exports = router;
