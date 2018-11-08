dff6c5b7bad42cf113215bcbcd160c4099d96afc
karim kamil <kkamil@eg.ibm.com>
2018-11-08 14:47:55 +0200

    change for port

--- models/makhdoumin.js
+++ models/makhdoumin.js
@@ -1,6 +1,9 @@
 var mongoose = require("mongoose");
-var MakhdouminSchema = new mongoose.Schema({
+var MakhdouminSchema = new mongoose.Schema(
+  {
     name: String,
-  address: String
-}, { collection: 'makhdoumin' });
+    address: String
+  },
+  { collection: "makhdoumin" }
+);
 module.exports = mongoose.model("makhdoumin", MakhdouminSchema);
