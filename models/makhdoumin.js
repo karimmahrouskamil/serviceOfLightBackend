21ef38fabe69888424502909903e4bee2892663c
karim kamil <kkamil@eg.ibm.com>
2018-11-16 15:54:16 +0200

    data restructure

--- models/makhdoumin.js
+++ models/makhdoumin.js
@@ -4,6 +4,7 @@ const AddressSchema = new mongoose.Schema({
   streetName: String,
   floorNumber: Number,
   flatNumber: Number,
+  zone: String,
   remarks: String
 }, {
   _id: false,
@@ -13,26 +14,44 @@ const PhonesSchema = new mongoose.Schema({
   MobilePhone: Number,
   HomePhone: Number,
   fatherPhone: Number,
-  motherPhone: Number
+  motherPhone: Number,
 }, {
   _id: false,
-  name: 'Address'
+  name: 'Mobiles'
 });
-
-var MakhdouminSchema = new mongoose.Schema({
+const OnlineSchema = new mongoose.Schema({
+  facebook: String,
+  email: String
+}, {
+  _id: false,
+  name: 'Online'
+});
+const PersonalSchema = new mongoose.Schema({
   name: String,
   address: AddressSchema,
   Mobiles: PhonesSchema,
-  zone: String,
-  class: Number,
+  onlineInfo: OnlineSchema,
   birthDate: Date,
+  school: String
+
+}, {
+  _id: false,
+  name: 'Personal'
+});
+const churchAndSpiritualInfoSchema = new mongoose.Schema({
+  class: Number,
   fatherOfConfession: String,
   servantFollowing: String,
   chomosya: String,
-  school: String,
-  facebook: String,
-  email: String,
-  Notes: String,
+  Notes: String
+
+}, {
+  _id: false,
+  name: 'Personal'
+});
+var MakhdouminSchema = new mongoose.Schema({
+  personalInfo: PersonalSchema,
+  churchAndSpiritualInfo: churchAndSpiritualInfoSchema,
   DateOfCreation: {
     type: Date,
     default: Date.now
