b657edfa6852c35efa85eec9ed677e1bb3f96543
karim kamil <kkamil@eg.ibm.com>
2018-11-16 15:11:23 +0200

    schema and data modeling

--- models/makhdoumin.js
+++ models/makhdoumin.js
@@ -1,11 +1,29 @@
 var mongoose = require("mongoose");
-var MakhdouminSchema = new mongoose.Schema(
-  {
-    name: String,
-    address: String,
-    phone : String,
-    HomePhone: String
-  },
-  { collection: "makhdoumin" }
-);
+var MakhdouminSchema = new mongoose.Schema({
+  name: String,
+  address: String,
+  MobilePhone: Number,
+  HomePhone: Number,
+  zone: String,
+  class: Number,
+  birthDate: Date,
+  fatherOfConfession: String,
+  servantFollowing: String,
+  chomosya: String,
+  school: String,
+  facebook: String,
+  email: String,
+  fatherPhone: Number,
+  motherPhone: Number,
+  Notes: String,
+  DateOfCreation: { type: Date, default: Date.now }
+
+}, {
+  collection: "makhdoumin"
+});
+// const AddressSchema = new mongoose.Schema({
+//  floorNumber: Number,
+//  streetAddress: String
+// }, { _id: false, name: 'Address'  });
+
 module.exports = mongoose.model("makhdoumin", MakhdouminSchema);
